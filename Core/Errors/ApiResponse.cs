ff73e8a5c1a9e0ca9b1431f8996452424c4d1d6c
Muhammed Hedia <hedyia@hotmail.com>
2020-09-27 09:57:06 -0700

    initial commit

---
+++ Core/Errors/ApiResponse.cs
@@ -0,0 +1,27 @@
+namespace Core.Errors
+{
+    public class ApiResponse
+    {
+        public ApiResponse(int statusCode, string message = null)
+        {
+            StatusCode = statusCode;
+            Message = message ?? GetDefaultMessageResponse(statusCode);
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+        private string GetDefaultMessageResponse(int statusCode)
+        {
+            return statusCode switch
+            {
+                400 => "Bad Request",
+                401 => "UnAuthorized",
+                404 => "Not Found",
+                500 => "Internal Server Error",
+                _ => null
+            };
+        }
+
+    }
+}
